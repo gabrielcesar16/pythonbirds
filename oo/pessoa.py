d7c36fcae01cf5e3afda46f9b272652415eb2ef2
gabrielcesar16 <gabrielcesar16@gmail.com>
2023-03-05 20:41:41 -0300

    atritbutos de instancia, nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 23):
+        self.idade = idade
+        self.nome = nome
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '  main  ':
-    p = Pessoa()
+    p = Pessoa('Gabriel')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)

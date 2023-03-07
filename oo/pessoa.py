34abab0c2140a0c82a23078f869838bc8da49634
gabrielcesar16 <gabrielcesar16@gmail.com>
2023-03-07 18:18:27 -0300

    Atributo complexo, filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 23):
+    def __init__(self, *filhos, nome = None, idade = 23):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-if __name__ == '  main  ':
-    p = Pessoa('Gabriel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+if __name__ == '__main__':
+    jose = Pessoa(nome="José")
+    joao = Pessoa(jose, nome='João')
+    print(id(jose))
+    print(jose.nome)
+    print(jose.idade)
+    
+    for filho in joao.filhos:
+        print (f'Os filhos de João são : {filho.nome}')

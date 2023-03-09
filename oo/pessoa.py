f6dbdfcbae9f59c7a6313523f1ef30fda13cfe8c
gabrielcesar16 <gabrielcesar16@gmail.com>
2023-03-09 19:55:08 -0300

    Métodos de classe, decorator staticmethod e classmethod

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,6 +10,13 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod #decorator
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - Numero de olhos da espécie: {cls.olhos}'
+
 if __name__ == '__main__':
     jose = Pessoa(nome="José")
     joao = Pessoa(jose, nome='João')
@@ -28,3 +35,4 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(joao.olhos)
     print(jose.olhos)
+    print(Pessoa.nome_e_atributos_de_classe())
